83e3f078f54b2db5217ab911066b587e4945436b
Vladimir Sokolovskiy <versus.sokol@gmail.com>
2025-09-15 13:44:00 +0500

    add init.sh

---
+++ init.sh
@@ -0,0 +1,44 @@
+# yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# tools
+yay -S neovim \
+ttf-nerd-fonts-symbols \
+ghostty \
+keepassxc \
+lazygit \
+lazysql \
+lazydocker \
+lazyssh \
+k9s \
+fzf \
+yazi \
+go \
+golangci-lint \
+rust \
+python \
+nodejs \
+bottom \
+gdu \
+ripgrep \
+tree-sitter \
+git \
+curl \
+wget
+
+# env
+yay -S hyprland \
+hyprcwd \
+hypridle \
+hyprlock \
+hyprmocha \
+hyprpaper \
+waybar \
+wofi
+
+# work
+yay -S aws-cli \
+aws-session-manager-plugin \
+google-chrome \
+slack-desktop \
+postman
